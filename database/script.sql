1cf69b4cf0cca4134d8bea2946797e4ef2cc5515
Bogdan Zakharov <zaharovbogdan2005@yandex.ru>
2025-03-21 02:19:47 +0300

    Database: new table add to script

--- database/script.sql
+++ database/script.sql
@@ -2,16 +2,20 @@ use `project_for_op13`;
 
 create table if not exists `user` (
     `login` varchar(255),
-    `password_hash` varchar(32),
+    `password_hash` varchar(32), -- SHA-256 hash
     constraint `user_pk` primary key(`login`)
 );
 
--- drop table `user` cascade;
-
 create table if not exists `person` (
-    `id` varchar(255) primary key references `user`(`login`) on update cascade on delete restrict,
+    `user` varchar(255),
     `first_name` varchar(255),
     `middle_name` varchar(255),
     `last_name` varchar(255),
-    `birthdate` date
+    `birthdate` date,
+    constraint `person_pk` primary key (`user`),
+    constraint `person_user_user_login_fk` foreign key (`user`) references `user` (`login`) on update cascade on delete cascade
 );
+
+-- Test values
+insert into `user` values ('bzakharov', 'same_hash');
+insert into `person` (`user`, `first_name`) values ('bzakharov', 'Богдан');
